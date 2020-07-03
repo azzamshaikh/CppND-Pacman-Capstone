eb2ef91ff04f1478e54b41128a647bcdfc3f9e36
Azzam Shaikh <47488876+azzamshaikh@users.noreply.github.com>
2020-07-03 04:10:57 -0500

    Changed method for checking of invalid characters

--- src/startup.cpp
+++ src/startup.cpp
@@ -12,21 +12,27 @@ bool Startup::bootscreen() {
     std::cout << "What would you like to do?" << std::endl;
     std::cout << "1: Start a game" << std::endl;
     std::cout << "2: Quit" << std::endl;
-    std::cout << "Please enter a number corresponding to the options above! :D : ";
     int keyevent;
-    std::cin >> keyevent;
+
+    while (std::cout << "Please enter a number corresponding to the options above! :D :" && !(std::cin >> keyevent)) {
+        std::cin.clear(); //clear bad input flag
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
+        std::cout << "Invalid input! please re-enter a number.\n";
+    }
 
     if ((keyevent < 1) || (keyevent > 2)) {
         std::cout << "Wrong number! Please enter a valid number please!" << std::endl;
         return false;
     }
     switch (keyevent) {
-    case 1:
-        return true;
-        break;
-    case 2:
-        std::cout << "Thanks for playing! Quitting the game now!" << std::endl;
-        setQuitGame();
-        return false;
+        case 1:{
+            return true;
+            break;
+        }
+        case 2:{
+            std::cout << "Thanks for playing! Quitting the game now!" << std::endl;
+            setQuitGame();
+            return false;
+        }
     }
 }
