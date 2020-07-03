2118f4a98d62736ed2a7cc81234d1e45d1f9f017
Azzam Shaikh <47488876+azzamshaikh@users.noreply.github.com>
2020-07-03 03:36:24 -0500

    Added comments to the functions

--- src/startup.h
+++ src/startup.h
@@ -6,11 +6,11 @@ using std::vector;
 class Startup {
 public:
     Startup() {
-        quitgame = false;
+        quitgame = false; 
     }
-    bool bootscreen();
-    void setQuitGame() { quitgame = true; }
-    bool getQuitGame() { return quitgame; }
+    bool bootscreen(); // load a bootscreen at the start of the game
+    void setQuitGame() { quitgame = true; } // set the quitgame variable to true
+    bool getQuitGame() { return quitgame; } // return the quitgame variable
 private:
     bool static quitgame;
 
